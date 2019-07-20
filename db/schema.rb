26f36bd120612c99cabe7068677bc1768984eb41
Stimulationparalysis <jealousbeastim@yahoo.co.jp>
2019-07-20 19:58:22 +0900

    schemaファイル修正

--- db/schema.rb
+++ db/schema.rb
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2019_07_08_115023) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "customer_id", null: false
-    t.string "card_id"
+    t.string "card_id", null: false
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
